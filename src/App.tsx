b747a4d0026e77c9ed55697d030a1ad61997f973
Zakaria Mouhid <zakariamouhid@gmail.com>
2025-12-03 23:48:30 +0100

    Refactor index.html for improved SEO and update favicon; add Prettier for code formatting; restructure App component with Header, Nav, Main, and Footer components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,72 @@
+import "./App.css";
+import logo from "./assets/logo.png";
 
-import './App.css'
-
-function App() {
-
+function Nav() {
   return (
-    <>
-      Homepage
-    </>
-  )
+    <nav>
+      <ul>
+        <li>
+          <a href="/" className="nav-link">
+            {" "}
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="/" className="nav-link">
+            {" "}
+            About
+          </a>
+        </li>
+        <li>
+          <a href="/" className="nav-link">
+            {" "}
+            Menu
+          </a>
+        </li>
+        <li>
+          <a href="/" className="nav-link">
+            {" "}
+            Contact
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
-export default App
+function Logo() {
+  return (
+    <a href="/" className="logo">
+      <img src={logo} alt="Little Lemon" width={200} height={55} />
+    </a>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <Logo />
+      <Nav />
+    </header>
+  );
+}
+
+function Main() {
+  return <main>Main</main>;
+}
+
+function Footer() {
+  return <footer>Footer</footer>;
+}
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
